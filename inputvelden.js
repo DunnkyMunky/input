d654809e2aa2a495b68a3068b1347881162b6893
DunnkyMunky <48646876+DunnkyMunky@users.noreply.github.com>
2021-09-15 09:09:12 +0200

    Add files via upload

---
+++ inputvelden.js
@@ -0,0 +1,8 @@
+function verrander() {
+if (document.getElementById("munten").value = "bitcoin") {
+    document.getElementById('munt').style.display = "block";
+} else if (document.getElementById("munten").value = "ethereum") {
+    document.getElementById('munt').style.display = "block";
+} else if (document.getElementById("munten").value = "litecoin") {
+	document.getElementById('munt').style.display = "block";}
+}
